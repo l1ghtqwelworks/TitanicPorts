9ccd960b626339f2852f50bb1a6203283dfd6f4c
SuprOrel <SuprOrel@gmail.com>
2021-12-03 01:51:19 +0200

    started empty console project, copied shipwreck lib and updated some packages

---
+++ Titanic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Titanic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteArgs(args);
+        }
+
+        static void ExecuteArgs(string[] args)
+        {
+            Console.WriteLine("Sailing Titanic");
+            string command = args[0];
+            switch (command)
+            {
+                case "get":
+                    string target = args[1];
+                    switch (target)
+                    {
+                        case "all":
+                            Console.WriteLine("All");
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+}
